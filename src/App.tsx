1bbd09d006a88ce5865f9e556aabb70ce075ef29
Diego <diannewdev@gmail.com>
2024-09-30 16:30:51 -0400

    initial commit, tailwind installed

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+
+    return (
+        <div className='bg-red-100'>
+            <p className='text-5xl'>Olá!</p>
+        </div>
+    );
+};
+
+export default App;
+
