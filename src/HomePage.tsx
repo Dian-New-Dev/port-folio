ccd9478ba236e6862bba8956f3347fa0d36b90fe
Diego <diannewdev@gmail.com>
2024-10-04 13:16:19 -0400

    base para o tutorial de react router pronta, fresh start

---
+++ src/HomePage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+
+    return (
+        <div className=''>
+            <p className=''>Home Page!</p>
+        </div>
+    );
+};
+
+export default HomePage;
+
---
+++ src/ProfilesPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const profiles = [1, 2, 3, 4, 5];
+
+const ProfilesPage: React.FC = () => {
+
+    return (
+        <div className=''>
+            <p className=''>Profiles Page!</p>
+        </div>
+    );
+};
+
+export default ProfilesPage;
+
