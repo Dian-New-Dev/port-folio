1038010b156e3e07d119e690867ed2dcfaf97d6b
Diego <diannewdev@gmail.com>
2024-10-04 13:20:54 -0400

    npm react router added and pages folder created

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HomePage from './HomePage.tsx'
+import HomePage from './pages/HomePage.tsx'
+import { createBrowserRouter } from 'react-router-dom'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
