e27d6060674b114324352bb397781004edf09cd7
Diego <diannewdev@gmail.com>
2024-11-04 00:19:47 -0400

    removi fundo animado da renderizacao para trabalhar nele

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,8 @@ import FundoAnimado from "./components/FundoAnimado";
 
 const App: React.FC = () => {
 
+    const mostrarFundoAnimado:boolean = false;
+
     return (
         <div className='relative grid place-items-center gradient-hero'>
             <div className='scroll-smooth p-4 sm:p-16 relative flex flex-col gap-16 2xl:gap-64 w-full max-w-[1440px] z-20 overflow-hidden'>
@@ -29,7 +31,7 @@ const App: React.FC = () => {
             </div>
 
             <div className="absolute top-0 left-0 h-full w-full">
-                <FundoAnimado />
+                {mostrarFundoAnimado && <FundoAnimado />}
             </div>
         
         </div>
